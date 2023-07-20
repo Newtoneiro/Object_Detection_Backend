54755414bfc31e0aeae9e3832e132c1df73468db
blatosek <01158787@pw.edu.pl>
2023-07-20 19:15:30 +0200

    dev

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
 import base64
+from ultralytics import YOLO
+import cv2
 
-HOST = "10.1.254.188"
+HOST = "10.1.7.78"
 PORT = 8888
 app = Flask(__name__)
+model = YOLO("yolov8n.pt")
 
 
 @app.route("/", methods=["GET"])
@@ -17,15 +20,22 @@ def serve_home():
 def serve_sampleResponse():
     data = request.json
     file = data['file']
-    imgdata = base64.b64decode(file + "==")
+    imgdata = base64.b64decode(file)
     filename = 'some_image.jpg'
     with open(filename, 'wb') as f:
         f.write(imgdata)
+    results = model.predict("some_image.jpg")
+    boxes = results[0].boxes.xywhn.numpy()
+    print(boxes)
+    boxes_json = [{"x": str(x), "y": str(y), "w": str(w), "h": str(h)}
+                  for (x, y, w, h) in boxes]
+    predicted_image = results[0].plot()
+    cv2.imwrite('./some_prediction.jpg', predicted_image)
     return jsonify({
-        'response': "Successfully received photo."
+        'result_boxes': boxes_json
     })
 
 
 if __name__ == "__main__":
     print(f"Server will be running on {HOST}:{PORT}")
-    app.run(host=HOST, port=PORT)
+    app.run(debug=True, host=HOST, port=PORT)
